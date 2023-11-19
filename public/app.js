6920bae684bc8f2bca9c749aad53de57abd2519c
Alex Fields <134031883+Zyvod@users.noreply.github.com>
2023-11-18 19:56:58 -0600

    Created get all data functionality to retrieve Database info at the loading of the webpage

--- public/app.js
+++ public/app.js
@@ -11,6 +11,23 @@ const userData = {
   userPassword: '123abc'
 }
 
+let partsData
+let cpuData
+let boardData
+let coolerData
+let gpuData
+let ramData
+let ssdData
+let caseData
+let powerData
+
+getAllData();
+
+homeBtn.on('click', () => {
+  $(browseSelect).hide();
+  welcomeMessage.show();
+})
+
 signBtn.on('click', async (e) => {
   console.log('Btn worked');
 
@@ -35,157 +52,98 @@ browseBtn.on('click', (e) => {
 async function listOfParts() {
   const browseSelect = $('<div>')
     browseSelect.attr('id', 'browseSelect')
+  while(browseSelect.children(0).length > 0) {
+    browseSelect.empty();
+  }
 
   const cpuPanel = $('<div>')
     cpuPanel.attr('class','panel')
-    cpuPanel.text('CPU')
   const cpuBtn = $('<button>')
+    cpuBtn.text('Browse CPUs')
+    cpuBtn.attr('class','panelBtn')
     cpuBtn.on('click', async (e) => {
-      try {
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/CPU',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      console.log(e.target.textContent)
+      populateResults(browseSelect, e.target.textContent)
     })
     cpuPanel.append(cpuBtn)
 
   const boardPanel = $('<div>')
     boardPanel.attr('class','panel')
-    boardPanel.text('Motherboard')
   const boardBtn = $('<button>')
+    boardBtn.text('Browse Motherboards')
+    boardBtn.attr('class','panelBtn')
     boardBtn.on('click', async (e) => {
-      try {
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/MOTHERBOARDS',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      console.log(e.target.textContent)
     })
     boardPanel.append(boardBtn)
 
   const coolerPanel = $('<div>')
     coolerPanel.attr('class','panel')
-    coolerPanel.text('CPU Cooler')
   const coolerBtn = $('<button>')
+    coolerBtn.text('Browse CPU Coolers')
+    coolerBtn.attr('class','panelBtn')
     coolerBtn.on('click', async (e) => {
-      try {
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/CPUCOOLERS',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      console.log(e.target.textContent)
     })
     coolerPanel.append(coolerBtn)
 
   const gpuPanel = $('<div>')
     gpuPanel.attr('class','panel')
-    gpuPanel.text('Graphics Card')
   const gpuBtn = $('<button>')
+    gpuBtn.text('Browse GPUs')
+    gpuBtn.attr('class','panelBtn')
     gpuBtn.on('click', async (e) => {
-      try {
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/GPU',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      console.log(e.target.textContent)
     })
     gpuPanel.append(gpuBtn)
 
   const ramPanel = $('<div>')
     ramPanel.attr('class','panel')
-    ramPanel.text('Memory')
   const ramBtn = $('<button>')
+    ramBtn.text('Browse Memory')
+    ramBtn.attr('class','panelBtn')
     ramBtn.on('click', async (e) => {
-      try {
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/RAM',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      console.log(e.target.textContent)
     })
     ramPanel.append(ramBtn)
 
   const ssdPanel = $('<div>')
     ssdPanel.attr('class','panel')
-    ssdPanel.text('Storage Device')
   const ssdBtn = $('<button>')
+    ssdBtn.text('Browse Storage')
+    ssdBtn.attr('class','panelBtn')
     ssdBtn.on('click', async (e) => {
-      try {
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/SSD',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      console.log(e.target.textContent)
     })
     ssdPanel.append(ssdBtn)
 
   const casePanel = $('<div>')
     casePanel.attr('class','panel')
-    casePanel.text('Case')
   const caseBtn = $('<button>')
+    caseBtn.text('Browse Cases')
+    caseBtn.attr('class','panelBtn')
     caseBtn.on('click', async (e) => {
-      try {
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/CASES',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      console.log(e.target.textContent)
     })
     casePanel.append(caseBtn)
 
   const powerPanel = $('<div>')
     powerPanel.attr('class','panel')
-    powerPanel.text('Power Supply')
   const powerBtn = $('<button>')
+    powerBtn.text('Browse PSUs')
+    powerBtn.attr('class','panelBtn')
     powerBtn.on('click', async (e) => {
-      try {
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/POWER',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      console.log(e.target.textContent)
     })
     powerPanel.append(powerBtn)
 
   const allPanel =$('<div>')
     allPanel.attr('class','panel')
-    allPanel.text('Browse All')
   const allBtn = $('<button>')
+    allBtn.text('Browse All')
+    allBtn.attr('class','panelBtn')
     allBtn.on('click', async (e) => {
-      try{
-        const response = await $.ajax({
-          url: 'http://localhost:3000/api/ALL',
-          type: 'GET'
-        })
-        console.log(response)
-      } catch(error) {
-        console.error('Error:',error)
-      }
+      populateResults(browseSelect,e.target.textContent)
     })
     allPanel.append(allBtn)
 
@@ -201,7 +159,75 @@ async function listOfParts() {
   container.append(browseSelect)
 }
 
-homeBtn.on('click', () => {
-  $(browseSelect).hide();
-  welcomeMessage.show();
-})
+async function populateResults(resultContainer,content) {
+  while (resultContainer.children().length > 0) {
+    resultContainer.empty();
+// console.log(typeof content)
+// console.log(cpuData)
+  switch (content) {
+    case 'Browse CPUs':
+            console.log(cpuData)
+    case 'Browse Motherboards':
+
+    case 'Browse CPU Coolers':
+        return function () {
+            console.log(coolerData);
+        };
+    case 'Browse GPUs':
+        return function () {
+            // Callback function for GPUs
+            console.log(gpuData);
+        };
+    case 'Browse Memory':
+        return function () {
+            // Callback function for Memory
+            console.log(ramData);
+        };
+    case 'Browse Storage':
+        return function () {
+            // Callback function for Storage
+            console.log(ssdData);
+        };
+    case 'Browse Cases':
+        return function () {
+            // Callback function for Cases
+            console.log(caseData);
+        };
+    case 'Browse PSUs':
+        return function () {
+            // Callback function for PSUs
+            console.log(powerData);
+        };
+    case 'Browse All':
+        return function () {
+            // Callback function for All
+            console.log(partsData);
+        };
+    default:
+        return function () {
+            // Default Callback function
+            console.log('Default Callback');
+        };
+}
+}}
+
+async function getAllData() {
+  try{
+    const response = await $.ajax({
+      url: 'http://localhost:3000/api/ALL',
+      type: 'GET'
+    })
+    partsData = response
+    cpuData = response['CPU']
+    boardData = response.Boards
+    coolerData = response.Coolers
+    gpuData = response['GPU']
+    ramData = response['RAM']
+    ssdData = response['SSD']
+    caseData = response.Cases
+    powerData = response.Power
+    console.log(partsData)
+  } catch(error) {
+    console.error('Error:',error)
+  }
+}
